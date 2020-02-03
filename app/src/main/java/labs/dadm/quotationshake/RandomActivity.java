7f6abff70b320a6366d735f881823a1f8291c420
Alejandro Gómez <algono@inf.upv.es>
2020-02-03 18:04:18 +0100

    Initial sample fetching random quotation

--- app/src/main/java/labs/dadm/quotationshake/RandomActivity.java
+++ app/src/main/java/labs/dadm/quotationshake/RandomActivity.java
@@ -1,14 +1,42 @@
 package labs.dadm.quotationshake;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RandomActivity extends AppCompatActivity {
 
+    private static final String USERNAME_PLACEHOLDER = "%1s", DEFAULT_USERNAME = "Nameless One";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
+
+        TextView textView = findViewById(R.id.textViewRandomQuote);
+        textView.setText(
+                textView.getText().toString().replace(USERNAME_PLACEHOLDER, DEFAULT_USERNAME));
+    }
+
+    public void onRandomQuote(View view) {
+        onStartFetchingRandomQuote();
+
+        TextView randomQuoteTextView = findViewById(R.id.textViewRandomQuote);
+        TextView randomAuthorTextView = findViewById(R.id.textViewRandomAuthor);
+
+        randomQuoteTextView.setText(R.string.random_sample_quote);
+        randomAuthorTextView.setText(R.string.random_sample_author);
+
+        onEndFetchingRandomQuote();
+    }
+
+    private void onStartFetchingRandomQuote() {
+        findViewById(R.id.progressBarRandomQuote).setVisibility(View.VISIBLE);
+    }
+
+    private void onEndFetchingRandomQuote() {
+        findViewById(R.id.progressBarRandomQuote).setVisibility(View.INVISIBLE);
     }
 }
