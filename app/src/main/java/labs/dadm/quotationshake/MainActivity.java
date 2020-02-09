6bd2a9b0e80f602d5e1298498c4f195136ff6ae9
Alejandro Gómez <algono@inf.upv.es>
2020-02-09 19:34:10 +0100

    Added some documentation to MainActivity Specifically, documentation regarding the use of a map when navigating to other activities

--- app/src/main/java/labs/dadm/quotationshake/MainActivity.java
+++ app/src/main/java/labs/dadm/quotationshake/MainActivity.java
@@ -9,8 +9,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // SparseArray<T> is equivalent to HashMap<Integer, T> (but better)
-    private static final SparseArray<Class> navButtonIdMap = createNavButtonIdMap();
+    /**
+     * <p>The reasoning behind using this is avoiding using a switch statement when navigating
+     * between activities.</p>
+     * <p></p>
+     * <p>As the code executed is always the same (navigate to the activity whose class is 'x')
+     * we map each button id with the activity it navigates to,
+     * so we can access it through 'map.get(...)' more easily
+     * (which is probably faster than the switch statement).</p>
+     * <p></p>
+     * <p>Note: {@link SparseArray}{@literal <T>} is equivalent to
+     * {@link java.util.HashMap}{@literal <Integer, T>} (but more memory-efficient)</p>
+     *
+     * @see #createNavButtonIdMap()
+     * @see #onNavigationButtonPressed(View)
+     */
+    public static final SparseArray<Class> navButtonIdMap = createNavButtonIdMap();
 
     /**
      * Don't use this method directly. Call the private field 'navButtonIdMap' instead.
@@ -33,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * <p>The reasoning behind using {@link #navButtonIdMap} is avoiding using a switch statement
+     * when navigating between activities.</p>
+     * <p></p>
+     * <p>For an extended explanation, check this: {@link #navButtonIdMap}.</p>
+     * @param v The view associated with the button press, usually the button we pressed
+     * @see #navButtonIdMap
+     */
     public void onNavigationButtonPressed(View v) {
         Class nextActivityClass = navButtonIdMap.get(v.getId());
         Intent intent = new Intent(this, nextActivityClass);
